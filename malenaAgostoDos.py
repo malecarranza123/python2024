60f1bd7b2fd212458a7636fcea55dec755ef186b
malecarranza123 <162601009+malecarranza123@users.noreply.github.com>
2024-08-12 12:09:58 -0300

    Add files via upload

---
+++ malenaAgostoDos.py
@@ -0,0 +1,42 @@
+# Diseñar un programa que permita al usuario gestionar una lista de tareas pendientes.
+
+tareas={}
+
+while True:
+         print( '''
+          #############################################
+          #     1 - Añadir una tarea                  #
+          #     2 - Ver todas las tareas              #
+          #     3 - Marcar una tarea como completada  #
+          #     4-  Salir                             #
+          ############################################# ''')
+
+         opcion = int(input("Ingrese el número de la opción que desea realizar: "))
+
+         if opcion == 1:
+            titulo = input("Ingrese el titulo de la tarea: ")
+            tarea = input("Ingrese la tarea: ")
+            tareas[titulo] = tarea
+            print("tarea agregada.")
+         
+         elif opcion == 2:
+           if tareas:
+             print("Lista de tareas:")
+             for titulo, tarea in tareas.items(): 
+               print(f"Título: {titulo}, Tarea: {tarea}")
+           else:
+            print("La lista de tareas está vacía.")
+
+         elif opcion == 3:
+           titulo = input("Ingrese el titulo de la tarea que ya completó: ")
+           if titulo in tareas:
+             del tareas[titulo]
+             print("Tarea completada.")
+         
+         elif opcion == 4:
+            break
+         
+         else:
+            print("Opción no válida. Por favor, seleccione una opción válida.")
+
+print("gracias por usar este programa :D")
