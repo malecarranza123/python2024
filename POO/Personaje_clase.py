d94f019ce54b25a32314a965192539a5a78d7a1a
malecarranza123 <162601009+malecarranza123@users.noreply.github.com>
2024-09-02 12:18:33 -0300

    Add files via upload

---
+++ POO/Personaje_clase.py
@@ -0,0 +1,10 @@
+class Personaje:
+    estado=True
+
+    def __init__(self,nombre, altura, velocidad, resistencia, fuerza):
+        self.nombre=nombre
+        self.altura=altura
+        self.velocidad=velocidad
+        self.resistencia=resistencia
+        self.fuerza=fuerza
+
