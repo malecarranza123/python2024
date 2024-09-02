699f5d0bc0fbcf1a056bb74020b35cdde78f2897
malecarranza123 <162601009+malecarranza123@users.noreply.github.com>
2024-09-02 12:21:43 -0300

    Add files via upload

--- POO/carrera.py
+++ POO/carrera.py
@@ -2,25 +2,25 @@ from Personaje_clase import Personaje
 
 cantidadPersonaje=0
 while True:
-         print( '''
-          #############################################
-          #     1 - Añadir un personaje               #
-          #     2 - Jugar carrera                     #
-          #     3 - Salir                             #
-          ############################################# ''')
+    print( '''
+        #############################################
+        #     1 - Añadir un personaje               #
+        #     2 - Jugar carrera                     #
+        #     3 - Salir                             #
+        ############################################# ''')
 
-         opcion = int(input("Ingrese el número de la opción que desea realizar: "))
+    opcion = int(input("Ingrese el número de la opción que desea realizar: "))
 
-         if opcion == 1:    
-         cantidadPersonaje=cantidadPersonaje+1
-         #nombre,altura,velocidad,resistencia,fuerza    
-         p + cantidadPersonaje= Personaje(input())
+    if opcion == 1:    
+        cantidadPersonaje=cantidadPersonaje+1
+        #nombre,altura,velocidad,resistencia,fuerza    
+        p + cantidadPersonaje= Personaje(input())
 
-         elif opcion == 2:
-             print("en proceso")
+    elif opcion == 2:
+        print("en proceso")
 
-         elif opcion == 3:
-             break
+    elif opcion == 3:
+        break
 
 
          
