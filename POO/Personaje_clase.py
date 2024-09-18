8855073a729fa315e346e08d57928a27a3bc9004
malecarranza123 <162601009+malecarranza123@users.noreply.github.com>
2024-09-18 11:10:18 -0300

    Add files via upload

--- POO/Personaje_clase.py
+++ POO/Personaje_clase.py
@@ -1,5 +1,6 @@
 class Personaje:
     estado=True
+    vida=100
 
     def __init__(self,nombre, altura, velocidad, resistencia, fuerza):
         self.nombre=nombre
@@ -7,4 +8,40 @@ class Personaje:
         self.velocidad=velocidad
         self.resistencia=resistencia
         self.fuerza=fuerza
+        self.estado=Personaje.estado
 
+    def atacar(self, otro_personaje):
+        if self.estado: #si estoy vivo
+            danio=self.fuerza-(otro_personaje.resistencia)
+            danio=max(0,danio)#no puede ser negativo
+            print(f"{self.nombfre} ataca a {otro_personaje.nombre} causando {danio} daño")
+            otro_personaje.recibir_dano(danio)#llamando al metodo recibir_dano y la cantidad es = danio
+        else:
+            print(f"{self.nombre} está muerto y no puede atacar")
+#"cantidad" va a tomar el valor deL DANIO
+
+    def recibir_dano(self, cantidad):
+        if self.estado:
+            self.vida-=cantidad
+            print(f"{self.nombre} recibe {cantidad} de daño. Vida restante es {self.vida}")
+            if self.vida<=0:
+                self.vida=0
+                print(f"{self.nombre} ha muerto")
+            else:
+                print(f"{self.nombre} ya está muerto")    
+        
+        self.resistencia -= cantidad
+        if self.resistencia <= 0:
+            self.resistencia = 0
+            self.estado = False
+            print(f"{self.nombre} ha muerto.")
+
+    def mostrar_info(self):
+        estado = "vivo" if self.estado else "muerto"
+        print(f"Nombre: {self.nombre}")
+        print(f"Altura: {self.altura}")
+        print(f"Velocidad: {self.velocidad}")
+        print(f"Resistencia: {self.resistencia}")
+        print(f"Fuerza: {self.fuerza}")
+        print(f"Vida: {self.vida}")
+        print(f"Estado: {estado}")
--- POO/carrera.py
+++ POO/carrera.py
@@ -1,26 +1,48 @@
 from Personaje_clase import Personaje
 
 cantidadPersonaje=0
+personajes=[]
 while True:
     print( '''
         #############################################
         #     1 - Añadir un personaje               #
-        #     2 - Jugar carrera                     #
+        #     2 - Jugar                             #
         #     3 - Salir                             #
         ############################################# ''')
 
     opcion = int(input("Ingrese el número de la opción que desea realizar: "))
 
     if opcion == 1:    
-        cantidadPersonaje=cantidadPersonaje+1
         #nombre,altura,velocidad,resistencia,fuerza    
-        p + cantidadPersonaje= Personaje(input())
+        print("Ingrese los datos del personaje:")
+        nombre = input("Nombre: ")
+        altura = int(input("Altura(en cm): "))
+        velocidad = int(input("Velocidad(0/100): "))
+        resistencia = int(input("Resistencia(0/100): "))
+        fuerza = int(input("Fuerza(0/100): "))
+
+        nuevo_personaje=Personaje(nombre,altura,velocidad,resistencia,fuerza) 
+        personajes.append(nuevo_personaje)  
+
+        cantidadPersonaje+=1
+        print(f"El personaje {nuevo_personaje} ha sido creado con éxito")
+        print(f"Cantidad de personajes creados: {cantidadPersonaje}")
 
     elif opcion == 2:
-        print("en proceso")
+        if cantidadPersonaje==0:
+            print("No hay personajes creados para jugar")
+        else:
+            print("Iniciando pelea con los siguientes personajes: ")
+            for personaje in personajes:
+                print(f"- {personaje.nombre}")
+        continue
+        
 
     elif opcion == 3:
+        print("chau")
         break
+
+print("hasta la próxima")
 
 
          
